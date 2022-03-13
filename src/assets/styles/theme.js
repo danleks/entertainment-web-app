047d89f0a9009bd3e44c1eea193ce8f7e5371eca
danleks <danleks@pop-os.localdomain>
2022-03-13 15:18:43 +0100

    finish global styles

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,8 +1,21 @@
 export const theme = {
-    colors: {
-        //
+    c: {
+        red: '#FC4747',
+        darkBlue: '#10141E',
+        greyishBlue: '#5A698F',
+        semiDarkBlue: '#161D2F',
+        white: '#ffffff',
     },
-    fontSize: {
-        //
+    // 1rem == 10px
+    fs: {
+        xl: '3.2rem',
+        l: '2.4rem',
+        m: '1.8rem',
+        s: '1.5rem',
+        xs: '1.3rem',
+    },
+    fw: {
+        light: '300',
+        medium: '500',
     },
 };
