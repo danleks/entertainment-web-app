2cdb5e7a82e32c8a56ca52ce6df91c6ee35f52e8
danleks <danleks@pop-os.localdomain>
2022-03-17 22:18:17 +0100

    add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,27 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from 'assets/styles/GlobalStyles';
+import {theme} from 'assets/styles/theme';
+import { addDecorator } from '@storybook/react';
+// import { initializeWorker, mswDecorator } from 'msw-storybook-addon';
+
+// initializeWorker();
+// addDecorator(mswDecorator);
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
