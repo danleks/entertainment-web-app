1c2bc5c4fb7fe483a052d446c97ae642e459ecd1
danleks <danleks@pop-os.localdomain>
2022-03-19 12:40:16 +0100

    write tests

---
+++ src/components/molecules/Thumbnail/Thumbnail.test.js
@@ -0,0 +1,8 @@
+import Thumbnail from './Thumbnail';
+import { render, screen } from 'test-utils';
+
+describe('Thumbnail', () => {
+    it('renders the component', () => {
+        render(<Thumbnail />);
+    });
+});
