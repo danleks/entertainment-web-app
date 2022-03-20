6f9f0eb96a4cdbc395ed2c7862c77f76d3507149
danleks <danleks@pop-os.localdomain>
2022-03-20 11:58:35 +0100

    update thumbnail

--- src/components/organisms/Section/Section.js
+++ src/components/organisms/Section/Section.js
@@ -2,24 +2,26 @@ import Thumbnail from 'components/molecules/Thumbnail/Thumbnail';
 import SectionTitle from 'components/atoms/SectionTitle/SectionTitle';
 import React from 'react';
 import { MediaWrapper, SectionWrapper, MediaWrapperItem } from './Section.styles';
+import { data } from 'data/data';
 
 const Section = ({ trending, title }) => {
     return (
         <SectionWrapper trending={trending}>
             <SectionTitle trending={trending}>{title}</SectionTitle>
             <MediaWrapper trending={trending}>
-                <MediaWrapperItem>
-                    <Thumbnail trending={trending} />
-                </MediaWrapperItem>
-                <MediaWrapperItem>
-                    <Thumbnail />
-                </MediaWrapperItem>
-                <MediaWrapperItem>
-                    <Thumbnail />
-                </MediaWrapperItem>
-                <MediaWrapperItem>
-                    <Thumbnail />
-                </MediaWrapperItem>
+                {data.map((item) => {
+                    if (item.isTrending) {
+                        return (
+                            <MediaWrapperItem key={item.title}>
+                                <Thumbnail trending={trending} />
+                            </MediaWrapperItem>
+                        );
+                    } else {
+                        <MediaWrapperItem>
+                            <Thumbnail />
+                        </MediaWrapperItem>;
+                    }
+                })}
             </MediaWrapper>
         </SectionWrapper>
     );
--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,4 @@
-const data = [
+export const data = [
     {
         title: 'Beyond Earth',
         thumbnail: {
