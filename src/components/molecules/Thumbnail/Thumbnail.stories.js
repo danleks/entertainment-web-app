626eb66987596b6884cff01417036755a0382ac6
danleks <danleks@pop-os.localdomain>
2022-03-26 11:21:26 +0100

    update story

--- src/components/molecules/Thumbnail/Thumbnail.stories.js
+++ src/components/molecules/Thumbnail/Thumbnail.stories.js
@@ -2,12 +2,32 @@ import React from 'react';
 
 import Thumbnail from './Thumbnail';
 
+const item = {
+    title: 'Beyond Earth',
+    thumbnail: {
+        trending: {
+            small: require('assets/images/thumbnails/beyond-earth/trending/small.jpg'),
+            large: require('assets/images/thumbnails/beyond-earth/trending/large.jpg'),
+        },
+        regular: {
+            small: require('assets/images/thumbnails/beyond-earth/regular/small.jpg'),
+            medium: require('assets/images/thumbnails/beyond-earth/regular/medium.jpg'),
+            large: require('assets/images/thumbnails/beyond-earth/regular/large.jpg'),
+        },
+    },
+    year: 2019,
+    category: 'Movie',
+    rating: 'PG',
+    isBookmarked: false,
+    isTrending: true,
+};
+
 export default {
     title: 'Components/Molecules/Thumbnail',
     component: Thumbnail,
 };
 
-const Template = (args) => <Thumbnail {...args} />;
+const Template = (args) => <Thumbnail {...args} item={item} />;
 
 export const Trending = Template.bind({});
 Trending.args = {
@@ -15,5 +35,5 @@ Trending.args = {
 };
 export const Primary = Template.bind({});
 Primary.args = {
-    treding: false,
+    trending: false,
 };
